62d194d5dc34b1be5bf401ef5846a06300353cb0
Scott Sinclair <scottysinclair@gmail.com>
2016-11-29 23:37:34 +0100

    chart images working

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -4,6 +4,7 @@ var merge = require('webpack-merge')
 var utils = require('./utils')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -33,6 +34,9 @@ module.exports = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true
-    })
+    }),
+    new CopyWebpackPlugin([
+       { from: 'node_modules/amcharts3/amcharts', to: 'amcharts'}
+    ])
   ]
 })
