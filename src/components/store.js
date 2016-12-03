68da8887d9fe9fe4e45bb9d022bec7d23b7fbcf3
Scott Sinclair <scottysinclair@gmail.com>
2016-12-03 23:43:11 +0100

    report coming from server.

--- src/components/store.js
+++ src/components/store.js
@@ -1,84 +1,34 @@
 console.log('------------- PROCESSING STORE.JS -----------------------------')
 
-window.fetch('/report/1')
-.then(function (response) {
-  return response.json()
-})
-.then(function (report) {
-  console.log(report)
-})
-
-export default {
+const store = {
   state: {
     reports: {
       '1': {
         id: 1,
-        name: 'Report Types',
+        name: 'Placeholder',
         columns: [
           {
             name: 'Name',
-            type: 'string'
-          },
-          {
-            name: 'Last Generated',
             type: 'string'
           }
         ],
         data: [
          ['Prisoner\'s Dilemma Report', '2 mins ago']
-        ],
-        subReports: [2]
-      },
-      '2': {
-        id: 2,
-        name: 'TitForTat vs TitFor2Tats',
-        columns: [
-          {
-            name: 'Round',
-            type: 'number'
-          },
-          {
-            name: 'Payer A Card',
-            type: 'string'
-          },
-          {
-            name: 'Player B Card',
-            type: 'string'
-          },
-          {
-            name: 'Player A Balance',
-            type: 'number'
-          },
-          {
-            name: 'Player A Delta',
-            type: 'number'
-          },
-          {
-            name: 'Player B Balance',
-            type: 'number'
-          },
-          {
-            name: 'Player B Delta',
-            type: 'number'
-          }
-        ],
-        data: [
-         [1, 'COOPERATE', 'COOPERATE', 0, 300, 0, 300],
-         [2, 'COOPERATE', 'COOPERATE', 0, 600, 0, 600],
-         [3, 'COOPERATE', 'COOPERATE', 0, 900, 0, 900],
-         [4, 'COOPERATE', 'COOPERATE', 0, 1000, 0, 1000],
-         [5, 'COOPERATE', 'COOPERATE', 0, 1300, 0, 1300],
-         [6, 'COOPERATE', 'COOPERATE', 0, 1600, 0, 1600],
-         [7, 'COOPERATE', 'COOPERATE', 0, 1800, 0, 1800],
-         [8, 'COOPERATE', 'COOPERATE', 0, 2000, 0, 2000]
-        ],
-        parentReport: 1,
-        subReports: []
+        ]
       }
-
     },
     getReport: function (id) {
       return this.reports[ id ]
     }
   }
 }
+
+window.fetch('/gamesrv/report/1')
+.then(function (response) {
+  return response.json()
+})
+.then(function (report) {
+  store.state.reports[report.id + ''] = report
+})
+
+export default store
